a8848fafab6b5f8ea66d44411564bdbb630ad917
Andrei Lapets <a@lapets.io>
2020-09-16 23:42:27 -0400

    Explicit support for all built-in types; more exhaustive tests.

--- sizeof/sizeof.py
+++ sizeof/sizeof.py
@@ -29,7 +29,23 @@ def sizeof(obj, deep=False, _exclude=None):
     True
     >>> sys.getsizeof(123) == sizeof(123)
     True
+    >>> sys.getsizeof(123.0123) == sizeof(123.0123)
+    True
+    >>> sys.getsizeof(complex(2, -3)) == sizeof(complex(2, -3))
+    True
+    >>> sys.getsizeof(True) == sizeof(True)
+    True
+    >>> sys.getsizeof(None) == sizeof(None)
+    True
     >>> sizeof('ab') == sizeof('a') + 1
+    True
+    >>> sizeof(bytes([1, 2])) + 1 == sizeof(bytes([1, 2, 3]))
+    True
+    >>> sizeof(bytearray([1, 2])) == sizeof(bytearray([1, 2, 3]))
+    True
+    >>> mv0 = memoryview(bytes([1, 2]))
+    >>> mv1 = memoryview(bytes([1, 2, 3]))
+    >>> sizeof(mv0) == sizeof(mv1)
     True
     >>> xs = [1, 2, 3]
     >>> ys = {'a':1, 'b':2, 'c':3}
@@ -38,16 +54,31 @@ def sizeof(obj, deep=False, _exclude=None):
     True
     >>> sys.getsizeof(xs) < sizeof(xs, deep=True)
     True
+    >>> sizeof(set(xs), deep=True) > sys.getsizeof(set(xs))
+    True
+    >>> sizeof({123}) == sys.getsizeof({123})
+    True
+    >>> sizeof({123}, deep=True) > sys.getsizeof({123})
+    True
     >>> sizeof([xs], deep=True) > sys.getsizeof([xs])
     True
     >>> sizeof([xs, ys, zs], deep=True) > 2 * sizeof([xs, xs, xs], deep=True)
+    True
+    >>> sizeof((xs, ys, zs), deep=True) > 2 * sizeof((xs, xs, xs), deep=True)
     True
     >>> sizeof([xs, xs], deep=True) < sizeof([xs, [1, 2, 3]], deep=True)
     True
     >>> sizeof([xs], deep=True, _exclude=set([id(xs)])) == sizeof([xs])
     True
+    >>> sys.getsizeof(range(0,10)) == sizeof(range(0,10))
+    True
+    >>> sizeof(range(0,10), deep=True) > sizeof(range(0,10))
+    True
     """
-    if not deep or isinstance(obj, (str, bytes, bytearray, int, float)):
+    if not deep or isinstance(obj, (
+            int, float, complex, bool,
+            str, bytes, bytearray, memoryview
+        )):
         return sys.getsizeof(obj)
 
     _exclude = set() if _exclude is None else _exclude
